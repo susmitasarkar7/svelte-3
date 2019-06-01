77e6a6dba03e9136a2e136e88a4fb28155305757
Susmita Sarkar <sarkarsusmita253@gmail.com>
2019-06-01 14:21:09 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let name = 'world';
+
+	let rando;
+
+	$: result = Math.round(rando) ? 'winner' : 'looser' ;
+
+	function setrando() {
+		rando = Math.random();
+	}
+</script>
+
+<h1>Hello {name}!</h1>
+
+<p>The random number is {rando}</p>
+
+<hr>
+
+<p>{result}</p>
+<p>{result}</p>
+<p>{result}</p>
+
+<button on:click={setrando}>Randomize</button>
+
+<input bind:value={rando}>
