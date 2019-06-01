9928419512552ebc5ab1e6218f29c5eb016418a7
Susmita Sarkar <sarkarsusmita253@gmail.com>
2019-06-01 16:40:14 +0530

    array push and app is reacting

--- src/App.svelte
+++ src/App.svelte
@@ -3,32 +3,16 @@
 
 	export let name = 'world';
 
-	let rando;
-
-	$: result = Math.round(rando * 100) ;
+	let randos = [];
 
 	function setrando() {
-		rando = Math.random();
+		const rando = Math.random(Math.random() * 100);
+		randos = [...randos, rando]
 	}
 </script>
 
 <h1>Hello {name}!</h1>
 
-<p>The random number is {rando}</p>
-
 <hr>
 
-<button on:click={setrando}>Randomize</button>
-
-<p>Your score is {result}</p>
-
-{#if result > 75}
-	<p transition:fade>Big winner! top 25%</p>
-{:else if result > 50}
-	<p>Decent, top 50%</p>
-{:else}
-	<p
-		in:fly={{ x:1000, duration:500 }}
-		out:fly={{ x:-500, duration:500 }}
-	>Loser</p>
-{/if}
+<button on:click={setrando}>Push</button>
